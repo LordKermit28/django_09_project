1f3d2172fd4d8067301d62123e24edbc91afb8a3
blau <serzh.plichko@mail.ru>
2023-09-28 08:30:05 +1100

    fix serizalizer

--- course/serializers.py
+++ course/serializers.py
@@ -3,12 +3,8 @@ from rest_framework import serializers
 from course.models import Course
 
 
-class CourseSerializer(serializers.Serializer):
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = '__all__'
 
-    def create(self, validated_data):
-        preview = self.context['request'].data.get('preview')
-        course = Course.objects.create(preview=preview, **validated_data)
-        return course
